8c157de7becfa5d6d8c702015954c14e3c6241e7
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-27 19:41:15 +0000

    Apr 27, 2026, 3:41 PM

--- scr/main.py
+++ scr/main.py
@@ -8,3 +8,8 @@ APP_PATH = os.path.dirname(os.path.abspath(__file__))
 def get_data_path(filename: str) -> str:
     '''Returns the path to an asset file, given its filename.'''
     return os.path.join(APP_PATH, "data", filename)
+
+
+
+st.title("Personal Banking App")
+st.write("This app will help track deposits, withdrawals, and account balance.")
