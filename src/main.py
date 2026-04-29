73c4c348e2b7ee5df3ad40c67e137b7212d4edba
ToxicWaste18607 <Mielewski@outlook.com>
2026-04-29 21:17:26 +0000

    Apr 29, 2026, 5:17 PM

--- src/main.py
+++ src/main.py
@@ -9,16 +9,27 @@ def get_data_path(filename: str) -> str:
     '''Returns the path to an asset file, given its filename.'''
     return os.path.join(APP_PATH, "data", filename)
 
+class User():
+    def __init__(self,username,role):
+        self.username= username
+        self.role=role
+
+
+
 def login_page():
     st.header("Login Page")
 
     with st.form("login_form"):
-        user_name=st.text_input("Enter your username")
+        username=st.text_input("Enter your username")
         role = st.selectbox("Role", ["user", "admin"])
         submitted = st.form_submit_button("Login")
 
         if submitted:
-            st.write(f"Hello {user_name} you are a {role}.")
+            st.session_state.user = User(username,role)
+            st.success ("Logged in")
+
+def hello_page(): 
+    pass
 
 
 
