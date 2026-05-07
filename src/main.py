bcc0293c957531bba54061b0acee9d513da4db77
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-07 03:22:16 +0000

    May 6, 2026, 11:22 PM

--- src/main.py
+++ src/main.py
@@ -128,10 +128,20 @@ def hello_page():
 
 #app code here
 
-
 if "user" not in st.session_state:
-    st.session_state.user=None
-    login_page()
+    st.session_state.user = None
+
+if st.session_state.user is None:
+    choice = st.radio("Choose", ["Login", "Create Account"])
+
+    if choice == "Login":
+        login_page()
+    else:
+        create_new_account()
 
 else:
     hello_page()
+
+    if st.button("Logout"):
+        st.session_state.user = None
+        st.rerun()
