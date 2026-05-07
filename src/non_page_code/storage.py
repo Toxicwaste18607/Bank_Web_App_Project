f43b0593618f05744b8ca970177fe488e2ceb24f
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-07 07:34:09 +0000

    May 7, 2026, 3:34 AM

---
+++ src/non_page_code/storage.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import random
+import os
+import json
+
+
+
+def get_data_path(filename: str) -> str:
+    '''Returns the path to an asset file, given its filename.'''
+    return os.path.join(APP_PATH, "data", filename)
+
+
+def load_user_data():
+    path= get_data_path("user_data.json")
+
+    if not os.path.exists(path):
+        return {}
+    
+    with open (path,'r') as file:
+        return json.load(file)
+    '''Loads user data from the file'''
+
+
+def save_users(users):
+    path = get_data_path("user_data.json")
+
+    with open(path, "w") as file:
+        json.dump(users, file, indent=4)
+    '''Saves users data'''
+
+
+def create_user(password, name, email, role="user"):
+    users = load_user_data()
+
+    user_id = generate_user_id(users)
+
+    users[user_id] = {
+        "password": password,
+        "name": name,
+        "role": role,
+        "balance": 0,
+        "email": email
+    }
+
+    save_users(users)
+
+    return user_id
+    '''Makes new user'''
+
+
+def generate_user_id(users):
+    while True:
+        new_id = str(random.randint(100, 999))
+
+        if new_id not in users:
+            return new_id
