957797d6e4003d96c4da70532eb96758339226a7
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-07 07:59:03 +0000

    May 7, 2026, 3:59 AM

--- src/app_pages/Transactions.py
+++ src/app_pages/Transactions.py
@@ -2,20 +2,13 @@ import streamlit as st
 
 
 
-def withdrawal():
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.write("Left")
-
-    with col2:
-        st.write("Right")
+def withdrawal():pass
 
 def deposit():pass
 
 
 def show_transactions_page():
-
+    
     choice = st.radio ("Choose", ["Withdrawal","Deposit"])
     if choice == "Withdrawal":
         withdrawal()
