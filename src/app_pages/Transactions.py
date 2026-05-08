e1acbfbf89dd42e2f6349ffb7c1af5b5a1f9a353
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-08 04:40:48 +0000

    May 8, 2026, 12:40 AM

--- src/app_pages/Transactions.py
+++ src/app_pages/Transactions.py
@@ -38,7 +38,7 @@ def deposit(money_in):
 def withdrawal_page():
     with st.form("withdrawal_form"):
         
-        money_out=st.number_input_input("What is the amount you would like to withdraw.")
+        money_out=st.number_input("What is the amount you would like to withdraw.")
         summitted= st.form_submit_button("Confirm transaction")
 
         if summitted:
