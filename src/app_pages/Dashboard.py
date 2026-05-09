3a496a542408c541d816bf939425b4bba9271026
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-09 05:34:45 +0000

    May 9, 2026, 1:34 AM

--- src/app_pages/Dashboard.py
+++ src/app_pages/Dashboard.py
@@ -5,7 +5,7 @@ from .Transactions import *
 
 def show_dashboard_page():
     user = st.session_state.user
-    st.session_state.current_page=='home'
+    st.session_state.current_page ='home'
     st.title("Dashboard")
     st.write(f"Welcome, {user.name}")
 
@@ -15,16 +15,22 @@ def show_dashboard_page():
     with st.sidebar:
         st.write(f'Hello {user.name}.')
         if st.button('Home'):
-            st.session_state.current_page
+            st.session_state.current_page='home'
 
-        if st.button('Deposit'):pass
+        if st.button('Deposit'):
+            st.session_state.current_page='deposit'
 
-        if st.button('Withdraw'):pass
+        if st.button('Withdraw'):
+            st.session_state.current_page='withdraw'
 
         if st.button('Logout'):
             st.session_state.user = None
             st.rerun()
 
+        if st.session_state.current_page == 'home':
+            pass
+
+        if st.session_state.current_page =='deposit'
 
 
 
