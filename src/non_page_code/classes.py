840e95aab45e8519dc8ee1d7dde56608b6cc5155
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-09 06:48:57 +0000

    May 9, 2026, 2:48 AM

--- src/non_page_code/classes.py
+++ src/non_page_code/classes.py
@@ -8,6 +8,44 @@ class User:
         self.username=username
 
 
+        def withdrawal(money_out):
+            user = st.session_state.user
+            users = load_user_data()
+
+
+            temp_balance = user.balance
+
+            temp_balance-=money_out
+
+
+            if temp_balance < 0:
+                return None
+            else:
+                user.balance -= money_out
+                users[user.user_id]["balance"]=user.balance
+                save_users(users)
+            return user
+
+
+
+
+
+
+        def deposit(money_in):
+            user = st.session_state.user
+            users = load_user_data()
+
+
+            user.balance += money_in
+
+            users[user.user_id]["balance"] = user.balance
+            save_users(users)
+            return user
+
+
+
+
+
 class Admin:
     def __init__(self, user_id, name, username,role, balance, email):
         self.user_id = user_id
