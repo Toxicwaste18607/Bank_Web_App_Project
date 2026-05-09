59db380a6d80609feee9fcd71b6fcc34c5d88e24
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-09 06:37:15 +0000

    May 9, 2026, 2:37 AM

--- src/app_pages/Dashboard.py
+++ src/app_pages/Dashboard.py
@@ -5,17 +5,22 @@ from .Transactions import *
 
 def show_dashboard_page():
     left,mid,right=st.columns([1,2,1])
+    user = st.session_state.user
 
 
     user = st.session_state.user
     
-    if 
+    if user.role=='user':
 
-    with mid:
-        st.title("Dashboard")
-        st.write(f"Welcome, {user.name}")
+        with mid:
+            st.title("Dashboard")
+            st.write(f"Welcome, {user.name}")
 
-        st.write(f"Current Balance is ${user.balance:,.2f}")
+            st.write(f"Current Balance is ${user.balance:,.2f}")
+
+    
+    if user.role=='admin':
+        
 
 
 
