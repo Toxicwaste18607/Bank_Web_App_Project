17230b6321f959fc42480c9211aaa1a215a5aa23
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-09 04:39:49 +0000

    May 9, 2026, 12:39 AM

--- src/non_page_code/classes.py
+++ src/non_page_code/classes.py
@@ -1,10 +1,11 @@
 class User:
-    def __init__(self, user_id, name, role, balance, email):
+    def __init__(self, user_id, name, user_name,role, balance, email):
         self.user_id = user_id
         self.name = name
         self.role = role
         self.balance = balance
         self.email = email
-        
+        self.user_name=user_name
+
 
     def allow_pages():pass
