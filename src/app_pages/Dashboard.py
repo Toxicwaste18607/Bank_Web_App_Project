81caabc2ad31b357ea7ff468da9e4b52bf3324bd
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-09 06:17:56 +0000

    May 9, 2026, 2:17 AM

--- src/app_pages/Dashboard.py
+++ src/app_pages/Dashboard.py
@@ -13,12 +13,13 @@ def show_dashboard_page():
     user = st.session_state.user
     st.session_state.current_page ='home'
     st.title("Dashboard")
-    st.write(f"Welcome, {user.name}")
-
-    st.write(f"Current Balance is ${user.balance:,.2f}")
+   
 
     with mid:
-        
+        st.write(f"Welcome, {user.name}")
+
+        st.write(f"Current Balance is ${user.balance:,.2f}")
+
 
 
 
