3a82092b5250e5b679e22210ae953103dcb21d7e
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-09 06:49:53 +0000

    May 9, 2026, 2:49 AM

--- src/classes/user.py
+++ src/classes/user.py
@@ -42,16 +42,3 @@ class User:
             save_users(users)
             return user
 
-
-
-
-
-class Admin:
-    def __init__(self, user_id, name, username,role, balance, email):
-        self.user_id = user_id
-        self.name = name
-        self.role = role
-        self.balance = balance
-        self.email = email
-        self.username=username
-
