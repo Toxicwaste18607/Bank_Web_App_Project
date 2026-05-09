1738f17e42100bae339a1aab65bfdea2d0f7254d
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-09 05:46:26 +0000

    May 9, 2026, 1:46 AM

--- src/app_pages/miscellaneous.py
+++ src/app_pages/miscellaneous.py
@@ -49,34 +49,7 @@ def create_new_account():
 
 
 
-def app_():
-    user = st.session_state.user
 
-
-    #if user.check == "user":
-    with st.sidebar:
-        st.write(f'Hello {user.name}.')
-        if st.button('Home'):
-            st.session_state.current_page='home'
-
-        if st.button('Deposit'):
-            st.session_state.current_page='deposit'
-
-        if st.button('Withdraw'):
-            st.session_state.current_page='withdraw'
-
-        if st.button('Logout'):
-            st.session_state.user = None
-            st.rerun()
-        
-    if st.session_state.current_page == 'home':
-        show_dashboard_page()
-
-    if st.session_state.current_page =='deposit':
-        deposit_page()
-
-    if st.session_state.current_page == 'withdraw':
-        withdrawal_page()
 
 
 
