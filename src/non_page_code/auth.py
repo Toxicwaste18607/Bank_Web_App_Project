4364d19520dd4b743fdd4556c9a3db3ecb6001a9
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-09 04:58:13 +0000

    May 9, 2026, 12:58 AM

--- src/non_page_code/auth.py
+++ src/non_page_code/auth.py
@@ -10,15 +10,18 @@ def check_login(user_id, password):
     for user_id, user_data in users.items():
         user_data = users[user_id] 
 
-        if user_data["password"] == password:
-            return User(
-                user_id=user_id,
-                name=user_data["name"],
-                user_name=user_data["user_name"],
-                role=user_data["role"],
-                balance=user_data["balance"],
-                email=user_data["email"]
-            )
+        if user_id in users or user_data["username"]:
+            if user_data["password"] == password:
+                return User(
+                    user_id=user_id,
+                    name=user_data["name"],
+                    user_name=user_data["user_name"],
+                    role=user_data["role"],
+                    balance=user_data["balance"],
+                    email=user_data["email"]
+                )
+        
+        
         
 
     return None
