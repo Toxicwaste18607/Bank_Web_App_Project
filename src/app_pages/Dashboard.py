6b0127ac875869683b33ae6dba05f04994a264ab
ToxicWaste18607 <Mielewski@outlook.com>
2026-05-11 02:39:34 +0000

    May 10, 2026, 10:39 PM

--- src/app_pages/Dashboard.py
+++ src/app_pages/Dashboard.py
@@ -12,7 +12,7 @@ def show_dashboard_page():
     user = st.session_state.user
     recent_transactions=transactions[user.user_id][-3:]
 
-    
+
     
     if user.role=='user':
 
@@ -21,6 +21,8 @@ def show_dashboard_page():
             st.write(f"Welcome, {user.name}")
 
             st.write(f"Current Balance is ${user.balance:,.2f}")
+            for transactions in recent_transactions:
+                st.write(transaction)
 
 
 
